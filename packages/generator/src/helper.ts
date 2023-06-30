c10401b7486d2ed89fdb976135c8893a568ff35a
Kazuki1st <kazui.kanamori@l-is-b.com>
2023-06-30 12:05:10 +0900

    feat: fix overflow text & omit 「...」

--- packages/generator/src/helper.ts
+++ packages/generator/src/helper.ts
@@ -304,12 +304,27 @@ const drawInputByTextSchema = (arg: {
       const textWidth =
         fontValue.widthOfTextAtSize(splitedLine, size) +
         (splitedLine.length - 1) * characterSpacing;
+      const y = calcY(templateSchema.position.y, pageHeight, size) -
+          lineHeight * size * (inputLineIndex + splitedLineIndex + beforeLineOver) -
+        (lineHeight === 0 ? 0 : ((lineHeight - 1) * size) / 2)
+
+      const nextLineY = calcY(templateSchema.position.y, pageHeight, size) -
+        lineHeight * size * (inputLineIndex + splitedLineIndex + 1 + beforeLineOver) -
+        (lineHeight === 0 ? 0 : ((lineHeight - 1) * size) / 2)
+      
+      const corrValue = 1.8
+      const maxPositionY = calcY(templateSchema.position.y, pageHeight, size) - mm2pt(templateSchema.height) + (lineHeight * size) / corrValue
+
+      if (nextLineY < maxPositionY) {
+        if (input.split(/\r|\n|\r\n/g).length > 1) {
+            splitedLine = splitedLine.slice(0, -2);
+            splitedLine += '...';
+        }
+      }
+      if (y < maxPositionY) return
       page.drawText(splitedLine, {
         x: calcX(templateSchema.position.x, alignment, width, textWidth),
-        y:
-          calcY(templateSchema.position.y, pageHeight, size) -
-          lineHeight * size * (inputLineIndex + splitedLineIndex + beforeLineOver) -
-          (lineHeight === 0 ? 0 : ((lineHeight - 1) * size) / 2),
+        y,
         rotate,
         size,
         color,
